58ecee78ebebb67d96c292f3706f8090f6d9456a
Corey <cstults9690@gmail.com>
2023-09-19 16:06:00 -0400

    cleaned up added imports to both files that I added that we're not needed.

--- automaters/src/main/java/net/automaters/script/panel/AutomateRSPanel.java
+++ automaters/src/main/java/net/automaters/script/panel/AutomateRSPanel.java
@@ -40,10 +40,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
