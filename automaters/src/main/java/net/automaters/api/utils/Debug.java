94a25b07e044e78a459eae7b4a993d8424944969
Zackaery <131960484+Zackaery@users.noreply.github.com>
2023-09-22 21:45:06 -0500

    Updated debug method Debugs a log to your AutomatRS file Consecutive lines are conjoined to a single line with a count before it.

---
+++ automaters/src/main/java/net/automaters/api/utils/Debug.java
@@ -0,0 +1,50 @@
+package net.automaters.api.utils;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Debug {
+
+    private static String lastMessage = null;
+    private static int consecutiveCount = 0;
+
+
+
+    public static void debug(String message) {
+        String debugExportFolder = System.getProperty("user.home") + File.separator + ".openosrs" + File.separator + "data" + File.separator + "AutomateRS" + File.separator + "Debug Export";
+        File debugExport = new File(debugExportFolder);
+
+        if (!debugExport.exists()) {
+            debugExport.mkdirs();
+        }
+
+        String logFilePath = debugExportFolder + File.separator + "log.txt";
+
+        try (FileWriter fw = new FileWriter(logFilePath, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+            String timestamp = dateFormat.format(new Date());
+            String logMessage = timestamp + ": " + message;
+
+            if (message.equals(lastMessage)) {
+                consecutiveCount++;
+            } else {
+                if (consecutiveCount > 0) {
+                    out.println("[" + consecutiveCount + " times consecutively] " + lastMessage);
+                    consecutiveCount = 0;
+                }
+                out.println(logMessage);
+                lastMessage = message;
+                consecutiveCount = 0;
+                System.out.println("[AutomateRS] - " + message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
